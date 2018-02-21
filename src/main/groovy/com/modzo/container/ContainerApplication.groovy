4fdc9876d1fec2f13b09b2592a9aeaaaff2e3c31
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-02-21 21:46:22 +0200

    Initial

---
+++ src/main/groovy/com/modzo/container/ContainerApplication.groovy
@@ -0,0 +1,12 @@
+package com.modzo.container
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ContainerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ContainerApplication, args
+	}
+}
